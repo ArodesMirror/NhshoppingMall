7f9cdfd0fb6909a0ba935c53a99200ac5d13b927
Arodes <2326785563@qq.com>
2021-11-29 21:30:33 +0800

    Delete main/java directory

--- main/java/com/nhcar/utils/Const.java
+++
@@ -1,8 +0,0 @@
-package com.nhcar.utils;
-
-public class Const {
-    public  static final String SERVER_URL="http://110.64.48.115:8080/";
-    public  static final String PIC_URL="uploads/";
-    public  static final String SERVLET_URL="android/";
-
-}
